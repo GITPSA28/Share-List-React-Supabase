8d18a2e0b56b7a5d45dd5cc256989c4ac125e4f0
GITPSA28 <74608471+GITPSA28@users.noreply.github.com>
2025-08-23 04:35:09 +0530

    taillwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div>
-      <p className="text-slate-50">Share List</p>
+      <p className="p-4 px-7 pt-4 text-slate-500">Share List</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
