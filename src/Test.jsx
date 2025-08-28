2cc9a3fcb57bba0d4b7ac78c90c6ac497d4c2f02
GITPSA28 <74608471+GITPSA28@users.noreply.github.com>
2025-08-29 00:36:27 +0530

    Google Auth

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,10 +1,25 @@
 import { useState, useEffect } from "react";
 import supabase from "./services/supabase.js";
+import { logOut, loginGoogle } from "./services/apiAuth.js";
 
 function Test() {
   const [movies, setMovies] = useState([]);
   const [movieIds, setMovieIds] = useState([]);
   const [theme, setTheme] = useState("black");
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+
   useEffect(() => {
     async function getMovies() {
       let { data: list_movies, error } = await supabase
@@ -32,8 +47,51 @@ function Test() {
     getMovies();
   }, []);
 
+  if (!session)
+    return (
+      <>
+        <button
+          className="btn border-[#e5e5e5] bg-white text-black"
+          onClick={loginGoogle}
+        >
+          <svg
+            aria-label="Google logo"
+            width="16"
+            height="16"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <g>
+              <path d="m0 0H512V512H0" fill="#fff"></path>
+              <path
+                fill="#34a853"
+                d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+              ></path>
+              <path
+                fill="#4285f4"
+                d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+              ></path>
+              <path
+                fill="#fbbc02"
+                d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+              ></path>
+              <path
+                fill="#ea4335"
+                d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+              ></path>
+            </g>
+          </svg>
+          Login with Google
+        </button>
+      </>
+    );
+  console.log(session);
   return (
     <div className="" data-theme={theme}>
+      <h1>welcome {session?.user?.user_metadata?.name}</h1>
+      <button className="btn btn-primary" onClick={logOut}>
+        Log out
+      </button>
       {/* <select onChange={(e) => setTheme(e.target.value)}>
         <option value="light">Default</option>
         <option value="dark">Dark</option>
@@ -45,33 +103,33 @@ function Test() {
           Fav Movies
         </li>
         {/* 
-        adult:false
-backdrop_path:"/6MmYsaK6poR8f4R8jsBnJBU5tfd.jpg"
-belongs_to_collection:null
-budget:7000000
-genres:(2) [{…}, {…}]
-homepage:"https://www.filmnation.com/library/gifted"
-id:400928
-imdb_id:"tt4481414"
-origin_country:['US']
-original_language:"en"
-original_title:"Gifted"
-overview:"Frank, a single man raising his child prodigy niece Mary, is drawn into a custody battle with his mother."
-popularity:4.0583
-poster_path:"/9Ts7Vc4wLlpI9oox9mkVUE1tBHy.jpg"
-production_companies:(3) [{…}, {…}, {…}]
-production_countries:(2) [{…}, {…}]
-release_date:"2017-04-07"
-revenue:40300000
-runtime:101
-spoken_languages:[{…}]
-status:"Released"
-tagline:""
-title:"Gifted"
-video:false
-vote_average:8.004
-vote_count:5731
-        */}
+              adult:false
+              backdrop_path:"/6MmYsaK6poR8f4R8jsBnJBU5tfd.jpg"
+              belongs_to_collection:null
+              budget:7000000
+              genres:(2) [{…}, {…}]
+              homepage:"https://www.filmnation.com/library/gifted"
+              id:400928
+              imdb_id:"tt4481414"
+              origin_country:['US']
+              original_language:"en"
+              original_title:"Gifted"
+              overview:"Frank, a single man raising his child prodigy niece Mary, is drawn into a custody battle with his mother."
+              popularity:4.0583
+              poster_path:"/9Ts7Vc4wLlpI9oox9mkVUE1tBHy.jpg"
+              production_companies:(3) [{…}, {…}, {…}]
+              production_countries:(2) [{…}, {…}]
+              release_date:"2017-04-07"
+              revenue:40300000
+              runtime:101
+              spoken_languages:[{…}]
+              status:"Released"
+              tagline:""
+              title:"Gifted"
+              video:false
+              vote_average:8.004
+              vote_count:5731
+          */}
         {movies.map((movie) => (
           <li className="list-row my-2 items-center" key={movie.id}>
             <div className="flex items-center justify-center">
