3ee3c4c0ed6ff3ada52bb3c32238f98e7f7c39e6
GITPSA28 <74608471+GITPSA28@users.noreply.github.com>
2025-09-21 23:34:15 +0530

    general component for items

---
+++ src/components/AddItemToList.jsx
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import Modal from "../ui/Modal";
+import Spinner from "../ui/Spinner";
+import useItemInUsersList from "../features/lists/useItemInUsersList";
+import { useUpdateListItems } from "../features/lists/useUpdateUserList";
+
+export default function AddItemToList({
+  item,
+  type = "movie",
+  className,
+  children,
+  custom = false,
+}) {
+  return (
+    <Modal>
+      <Modal.ModalContent className={"text-base-content bg-base-100"}>
+        <h3 className="text-lg font-bold">
+          Add {type === "movie" && item.title}
+          {type === "tv" && item.name}
+        </h3>
+        <p className="py-4">Select Lists</p>
+        <AddToListContent type={type} item={item} custom={custom} />
+      </Modal.ModalContent>
+      <Modal.OpenModel
+        className={className ? className : "btn btn-sm btn-ghost"}
+      >
+        {children ? (
+          children
+        ) : (
+          <>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-info-icon lucide-info"
+            >
+              <path d="M5 12h14" />
+              <path d="M12 5v14" />
+            </svg>
+            Add
+          </>
+        )}
+      </Modal.OpenModel>
+    </Modal>
+  );
+}
+function AddToListContent({ item, type, custom }) {
+  const { lists, isLoading } = useItemInUsersList({
+    item: item.id,
+    type,
+    custom,
+  });
+  if (isLoading) return <Spinner />;
+  return (
+    <SelectList
+      type={type}
+      item={item}
+      userList={lists}
+      isLoading={isLoading}
+    />
+  );
+}
+
+function SelectList({ item, type, userList, isLoading }) {
+  const { isUpdating, updateList } = useUpdateListItems({ item: item.id });
+  const [selectedList, setSelectedList] = useState(() => {
+    const initLists = userList.map((list) => {
+      return {
+        checked: list.isExists,
+        initChecked: list.isExists,
+        list_name: list.list_name,
+        list_id: list.list_id,
+      };
+    });
+    return initLists;
+  });
+  function updateSelectedList(list_id, checked) {
+    setSelectedList((lists) => [
+      ...lists.map((list) => {
+        if (list.list_id !== list_id) return list;
+        return { ...list, checked };
+      }),
+    ]);
+  }
+  function handleSubmit() {
+    updateList({
+      value: item.id,
+      type: type,
+      lists: selectedList.filter((list) => list.initChecked !== list.checked),
+    });
+  }
+  if (isLoading) return <></>;
+  if (!selectedList.length) return <>No List found</>;
+  return (
+    <div>
+      <ul>
+        {selectedList.map((list) => {
+          return (
+            <fieldset key={list.list_id} className="fieldset m-2">
+              <label className="label text-base-content py-2">
+                <input
+                  type="checkbox"
+                  checked={list.checked}
+                  onChange={(e) =>
+                    updateSelectedList(list.list_id, e.target.checked)
+                  }
+                  className="checkbox"
+                />
+                {list.list_name}
+              </label>
+            </fieldset>
+          );
+        })}
+      </ul>
+      <Modal.ModalAction>
+        <Modal.ModalClose className={"btn btn-sm sm:btn-md"} />
+        {/* {selectedFriends.length > 0 && ( */}
+        <Modal.ModalClose
+          disabled={isUpdating}
+          submit={handleSubmit}
+          className="btn-primary btn btn-sm sm:btn-md"
+        >
+          Save
+        </Modal.ModalClose>
+        {/* )} */}
+      </Modal.ModalAction>
+    </div>
+  );
+}
---
+++ src/components/SendItem.jsx
@@ -0,0 +1,140 @@
+import React, { useState } from "react";
+import Modal from "../ui/Modal";
+import Avatar from "../ui/Avatar";
+import useFriendList from "../features/friends/useFriendList";
+import { useSession } from "../contexts/SessionContext";
+import { useSendItemsToFriends } from "../features/friends/useSendItemsToFreinds";
+import Spinner from "../ui/Spinner";
+
+export default function SendItem({
+  item,
+  type = "movie",
+  className,
+  children,
+}) {
+  return (
+    <Modal>
+      <Modal.ModalContent className={"text-base-content bg-base-100"}>
+        <h3 className="text-lg font-bold">
+          Send {type === "movie" && item.title}
+          {type === "tv" && item.name}
+        </h3>
+        <p className="py-4">Select friends</p>
+        <SendItemContent item={item} type={type} />
+      </Modal.ModalContent>
+      {/* <> */}
+
+      <Modal.OpenModel
+        className={
+          className ? className : "btn glass text-neutral-content btn-sm"
+        }
+      >
+        {children ? (
+          children
+        ) : (
+          <>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className=""
+            >
+              <path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z" />
+              <path d="m21.854 2.147-10.94 10.939" />
+            </svg>
+            Send
+          </>
+        )}
+      </Modal.OpenModel>
+
+      {/* </> */}
+    </Modal>
+  );
+}
+function SendItemContent({ item, type }) {
+  const { friendList, isLoading } = useFriendList();
+  return (
+    <SelectFriends
+      item={item}
+      type={type}
+      friendList={friendList}
+      isLoading={isLoading}
+    />
+  );
+}
+function SelectFriends({ item, type, friendList, isLoading }) {
+  const [selectedFriends, setSelectedFreinds] = useState([]);
+  const {
+    session: {
+      user: { id: user_id },
+    },
+  } = useSession();
+  const { isSending, sendItemsToFriendIds } = useSendItemsToFriends();
+
+  function addFriendId(id) {
+    setSelectedFreinds((ids) => [...ids, id]);
+  }
+  function removeFriendId(id) {
+    setSelectedFreinds((ids) => [...ids.filter((el) => el != id)]);
+  }
+  function handleSend() {
+    sendItemsToFriendIds({
+      value: item.id,
+      type: type,
+      friendIds: selectedFriends,
+    });
+  }
+  if (isLoading || isSending) return <Spinner />;
+  if (!friendList && !isLoading) return <p>No friends</p>;
+  return (
+    <>
+      <form>
+        <div className="flex flex-wrap gap-3">
+          {friendList?.map((friend) => {
+            return (
+              <label
+                className="text-center"
+                key={friend.id}
+                htmlFor={friend.id}
+              >
+                <div className="relative">
+                  <input
+                    className="checkbox checkbox-primary absolute right-0 bottom-0 z-20 border-0"
+                    type="checkbox"
+                    name="friend"
+                    id={friend.id}
+                    onChange={(e) => {
+                      if (e.target.checked) addFriendId(friend.id);
+                      else removeFriendId(friend.id);
+                    }}
+                  ></input>
+
+                  <Avatar className={"w-16 rounded-full"} user={friend} />
+                </div>
+                {friend.username}
+              </label>
+            );
+          })}
+        </div>
+
+        <Modal.ModalAction>
+          <Modal.ModalClose className={"btn btn-sm sm:btn-md"} />
+          {selectedFriends.length > 0 && (
+            <Modal.ModalClose
+              submit={handleSend}
+              className="btn-primary btn btn-sm sm:btn-md"
+            >
+              send
+            </Modal.ModalClose>
+          )}
+        </Modal.ModalAction>
+      </form>
+    </>
+  );
+}
