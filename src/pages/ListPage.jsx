8a2cc223f793538be466696f65696593edd9ca2a
GITPSA28 <74608471+GITPSA28@users.noreply.github.com>
2025-09-26 01:49:51 +0530

    List page render issue fix

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link, useParams } from "react-router";
-import { getListById } from "../services/apiUserList";
 import FullscreenSpinner from "../ui/FullscreenSpinner";
 import MovieCard from "../ui/MovieCard";
 import useListItemsDetails from "../features/tmdb/useListItemsDetails";
 import useListItems from "../features/lists/useListItems";
+import AddItemToList from "../components/AddItemToList";
+import SendItem from "../components/SendItem";
+import DeleteItemFromList from "../components/DeleteItemFromList";
+import { useSession } from "../contexts/SessionContext";
+import useItemDetails from "../features/tmdb/useItemDetails";
 
 export default function ListPage() {
   const { listid } = useParams();
@@ -19,13 +23,11 @@ export default function ListPage() {
 }
 
 function MovieList({ list }) {
-  const { listItems, isLoading } = useListItemsDetails({
-    items: list.items,
-    list_id: list.id,
-  });
-  if (isLoading) return <FullscreenSpinner />;
-  console.log(listItems);
-  if (!listItems?.length) return <>Empty</>;
+  const {
+    session: {
+      user: { id: user_id },
+    },
+  } = useSession();
   return (
     <div>
       <h2 className="font-bold tracking-wide uppercase">
@@ -36,17 +38,41 @@ function MovieList({ list }) {
         <em>{list.list_name}</em>
       </h2>
       <ul className={`bg-base-100 mt-3 flex flex-wrap justify-start gap-3`}>
-        {listItems.map((item) => (
-          <div className="max-w-xl sm:flex-2/5" key={item.id}>
-            <MovieCard
-              movie={item}
-              type={item.itemType}
-              showOverView={false}
-            ></MovieCard>
-            {/* <MovieCard movie={item} showOverView={false}></MovieCard> */}
-          </div>
+        {list?.items?.map((item) => (
+          <Item
+            key={item.id}
+            value={item.value}
+            type={item.type}
+            itemId={item.id}
+            showDelete={
+              list.owner_id === user_id || list.recommended_to === user_id
+            }
+          />
         ))}
       </ul>
+    </div>
+  );
+}
+
+function Item({ value, type, itemId, showDelete = false }) {
+  const { data: item, isLoading } = useItemDetails({
+    value,
+    type,
+  });
+  if (isLoading) return;
+  if (!item) return;
+  return (
+    <div className="max-w-xl sm:flex-2/5">
+      <MovieCard movie={item} type={type} showOverView={false}>
+        <AddItemToList item={item} type={type} />
+        <SendItem item={item} type={type} />
+        {showDelete && (
+          <DeleteItemFromList
+            className="btn btn-circle btn-ghost btn-sm btn-error"
+            item_id={itemId}
+          />
+        )}
+      </MovieCard>
     </div>
   );
 }
