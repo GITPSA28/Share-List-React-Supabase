62961ce4c855ecb17487d7af2c7893dce331413f
GITPSA28 <74608471+GITPSA28@users.noreply.github.com>
2025-09-26 01:48:00 +0530

    Delete Item using Id

--- src/features/lists/useDeleteItem.js
+++ src/features/lists/useDeleteItem.js
@@ -1,18 +1,25 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { deleteItemFromList } from "../../services/apiUserList";
+import { deleteItemUsingId } from "../../services/apiUserList";
 import toast from "react-hot-toast";
 
-export function useDeleteItem({ item, list_id }) {
+export function useDeleteItem({ item_id }) {
   const queryClient = useQueryClient();
   const { isLoading: isDeleting, mutate: deleteItem } = useMutation({
-    mutationFn: () => deleteItemFromList({ item, list_id }),
+    mutationFn: () => deleteItemUsingId({ item_id }),
     onMutate: () => {
       toast.loading("Deleting..", { id: "delete" });
     },
-    onSuccess: ({ data }) => {
+    onSuccess: () => {
       toast.success("Deleted Succesfully", { id: "delete" });
-      queryClient.invalidateQueries({ queryKey: ["user-lists"] });
-      queryClient.invalidateQueries({ queryKey: ["recommendations"] });
+      queryClient.invalidateQueries({
+        queryKey: ["user-lists"],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["recommendations"],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["list"],
+      });
     },
     onError: (error) => {
       toast.error("Error while Deleting", { id: "delete" });
