a2c675d2fba95cfa2a675720c3872246d40d173c
GITPSA28 <74608471+GITPSA28@users.noreply.github.com>
2025-09-26 01:45:58 +0530

    Auth Req Fix

--- src/features/authentication/useUser.js
+++ src/features/authentication/useUser.js
@@ -1,10 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCurrentUser } from "../../services/apiAuth";
+import { useSession } from "../../contexts/SessionContext";
 
 export function useUser() {
+  const {
+    session: { user: userData },
+  } = useSession();
   const { isLoading, data: user } = useQuery({
     queryKey: ["user"],
-    queryFn: getCurrentUser,
+    queryFn: () => {
+      return userData;
+    },
   });
   return {
     isLoading,
