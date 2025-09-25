a2ff59263f434e7cbb9f7db112fbd71f67c205d6
GITPSA28 <74608471+GITPSA28@users.noreply.github.com>
2025-09-26 01:48:16 +0530

    list item key

--- src/features/lists/useListItems.js
+++ src/features/lists/useListItems.js
@@ -4,15 +4,18 @@ import { getListById } from "../../services/apiUserList";
 export default function useListItems({ list_id }) {
   const {
     isLoading,
+    isRefetching,
     data: list,
     error,
   } = useQuery({
-    queryKey: ["list-items", list_id],
+    queryKey: ["list", "items", `${list_id}`],
     queryFn: () => getListById({ list_id }),
+    retry: false,
   });
 
   return {
     isLoading,
+    isRefetching,
     error,
     list,
   };
