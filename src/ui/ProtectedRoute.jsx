3bd38df83793d2c98668665250b51c56c5513070
GITPSA28 <74608471+GITPSA28@users.noreply.github.com>
2025-10-07 22:29:33 +0530

    protected route session update

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -2,10 +2,17 @@ import { useUser } from "../features/authentication/useUser";
 import { useNavigate } from "react-router";
 import { useEffect } from "react";
 import FullscreenSpinner from "./FullscreenSpinner";
+import { useSession } from "../contexts/SessionContext";
 
 function ProtectedRoute({ children }) {
-  const { isAuthenticated, isLoading } = useUser();
+  const {
+    session: {
+      user: { role },
+    },
+    isLoading,
+  } = useSession();
   const navigate = useNavigate();
+  const isAuthenticated = role === "authenticated" || false;
   useEffect(
     function () {
       if (!isLoading && !isAuthenticated) navigate("/login");
